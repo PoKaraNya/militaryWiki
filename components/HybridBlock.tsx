c1d948b886dd455148ed0779fc05122e8b104572
sergienko-anastasiya <vt211_sas@student.ztu.edu.ua>
2023-10-15 22:46:40 +0300

    useJson hook

--- components/HybridBlock.tsx
+++ components/HybridBlock.tsx
@@ -1,21 +1,32 @@
 import { FC } from 'react'
 import { BaseContentBlock } from '@/components/BaseContentBlock'
 import '@/styles/ContentBlocks/HybridBlock.scss'
+import { Skeleton } from 'antd'
 
 interface IProps {
-  text: string
-  img: string
+  text?: string
+  img?: string
   reverse?: boolean
+  loading?: boolean
 }
 
-export const HybridBlock: FC<IProps> = ({ text, img, reverse = false }) => {
+export const HybridBlock: FC<IProps> = ({
+  text = '',
+  img = '',
+  reverse = false,
+  loading = false,
+}) => {
   return (
     <BaseContentBlock className={'hybrid-block'}>
-      <div className="content-container">
-        {reverse || <p>{text}</p>}
-        {img ? <img src={img} alt="Photo" /> : null}
-        {reverse && <p>{text}</p>}
-      </div>
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <div className="content-container">
+          {reverse || <p>{text}</p>}
+          {img ? <img src={img} alt="Photo" /> : null}
+          {reverse && <p>{text}</p>}
+        </div>
+      )}
     </BaseContentBlock>
   )
 }
--- components/TransparentBlock.tsx
+++ components/TransparentBlock.tsx
@@ -1,17 +1,29 @@
 import { FC } from 'react'
 import { BaseContentBlock } from '@/components/BaseContentBlock'
 import '@/styles/ContentBlocks/TrancparentBlock.scss'
+import { Skeleton } from 'antd'
 
 interface IProps {
-  title: string
-  description: string
+  title?: string
+  description?: string
+  loading?: boolean
 }
 
-export const TransparentBlock: FC<IProps> = ({ title, description }) => {
+export const TransparentBlock: FC<IProps> = ({
+  title = '',
+  description = '',
+  loading = false,
+}) => {
   return (
     <BaseContentBlock transparent className={'transparent-block'}>
-      <h2>{title}</h2>
-      <h4>{description}</h4>
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <>
+          <h2>{title}</h2>
+          <h4>{description}</h4>
+        </>
+      )}
     </BaseContentBlock>
   )
 }
