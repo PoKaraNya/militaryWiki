a9b0ff43f41c33d09d7e4b9889d1eaf28696d6c5
zbun-karina <vt211_zks@student.ztu.edu.ua>
2023-10-15 17:47:31 +0300

    lazy loading, map and conditional rendering

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react'
+'use client'
+import {FC, Suspense, useState} from 'react'
 import { TransparentBlock } from '@/components/TransparentBlock'
 import {
   welcomeBlock,
@@ -10,10 +11,20 @@ import {
 } from '@/mock/mainPage.json'
 import { TextBlock } from '@/components/TextBlock'
 import { HybridBlock } from '@/components/HybridBlock'
+import dynamic from "next/dynamic";
+import Loading from "@/components/Loading";
+
+const DynamicVeryHeavyComponent = dynamic(() => import("@/components/VeryHeavyComponent"), {
+    suspense: true,
+    loading: () => <Loading/>
+})
 
 interface IProps {}
 
+
 const Home: FC<IProps> = () => {
+  const [isShow, setIsShow] = useState<boolean>(false)
+
   return (
     <main>
       <TransparentBlock
@@ -25,6 +36,11 @@ const Home: FC<IProps> = () => {
       <HybridBlock text={hybridBlock1.text} img={hybridBlock2.img} reverse />
       <HybridBlock text={hybridBlock1.text} img={hybridBlock3.img}/>
       <HybridBlock text={hybridBlock1.text} img={hybridBlock4.img} reverse/>
+        {
+            isShow
+                ? <DynamicVeryHeavyComponent/>
+                : <button onClick={() => setIsShow(true)}>Показаті</button>
+        }
     </main>
   )
 }
