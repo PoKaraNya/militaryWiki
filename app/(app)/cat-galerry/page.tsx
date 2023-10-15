d2273b9028da4c50cac2f687c6e6163f0ef3cbf7
zbun-karina <vt211_zks@student.ztu.edu.ua>
2023-10-15 20:11:35 +0300

    useApi hook and useRef implementation

---
+++ app/(app)/cat-galerry/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+import GalerryGridVirtualized from "@/components/GalerryGridVirtualized";
+import "@/styles/Galerry/main.scss"
+import {useApi} from "@/hooks/useApi";
+import {CAT_API} from "@/config/api";
+import {ICatApi} from "@/types/api";
+import Loading from "@/components/Loading";
+import {useRef} from "react";
+
+export default function CatGalerryPage() {
+    const {data, error, refetch, loading} = useApi<ICatApi[]>(CAT_API, true, 10)
+    const buttonRef = useRef<HTMLButtonElement>(null)
+    if (loading && !data) {
+        return <Loading/>
+    }
+
+    if (error) {
+        return <h2>Помилка: {error}</h2>
+    }
+
+    const handleFocus = () => {
+        buttonRef?.current?.focus()
+    }
+
+    const photos = (data ?? []).map(({url}) => url)
+
+    return (
+        <main className="gallery">
+            <button ref={buttonRef} onClick={() => refetch()}>
+                Оновити
+            </button>
+            <div className="container">
+                <GalerryGridVirtualized data={photos}/>
+            </div>
+            <button onClick={handleFocus}>
+                Фокус на кнопку оновлення
+            </button>
+        </main>
+    )
+}
