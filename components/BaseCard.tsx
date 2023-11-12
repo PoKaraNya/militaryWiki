178ccaf2de80de36d2b73fde386ef76a654759f6
sergienko-anastasiya <vt211_sas@student.ztu.edu.ua>
2023-11-12 16:45:41 +0200

    goods card add

---
+++ components/BaseCard.tsx
@@ -0,0 +1,21 @@
+import {FC} from "react";
+import s from '@/styles/Goods/Card.module.scss'
+interface IProps {
+    img?: string
+    title: string
+    description: string
+    price: number
+    clickHandle: () => void
+}
+
+const BaseCard: FC<IProps> = ({img, price, description, title, clickHandle}) => {
+    return (
+        <div className={s.card} onClick={clickHandle}>
+            <img className={s.preview} src={img} alt="preview"/>
+            <div className={s.title}>{title}</div>
+            <div className={s.description}>{description ?? ''}</div>
+            <div className={s.price}>{price + ' грн'}</div>
+        </div>
+    )
+}
+export default BaseCard
