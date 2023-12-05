6839a953f7e8a8a004ba352fa700fcb16c477435
zbun-karina <vt211_zks@student.ztu.edu.ua>
2023-12-05 22:49:05 +0200

    add goods features

--- app/(app)/goods/clothes/page.tsx
+++ app/(app)/goods/clothes/page.tsx
@@ -5,7 +5,7 @@ import BaseCardContainer from '@/components/BaseCardContainer'
 import {useJson} from "@/hooks/useJson";
 import {IGoodsCard} from "@/types/goods";
 import {useAppDispatch, useAppSelector} from "@/store/hooks";
-import {clearCart, toggleGoods, totalPriceComputed} from "@/store/slices/goodsSlice";
+import {buyCartOrFail, clearCart, reset, toggleGoods, totalPriceComputed} from "@/store/slices/goodsSlice";
 
 interface IProps {}
 
@@ -16,6 +16,7 @@ const ClothesPage: FC<IProps> = () => {
   const total = useAppSelector(totalPriceComputed)
   const cart = useAppSelector(state => state.goods.cart)
   const dispatch = useAppDispatch()
+  const money = useAppSelector(state => state.goods.money)
 
   if (!data) {
       return null
@@ -29,10 +30,27 @@ const ClothesPage: FC<IProps> = () => {
       dispatch(clearCart())
   }
 
+  function buyCartHandle() {
+    try {
+        dispatch(buyCartOrFail())
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            alert(e.message)
+        }
+    }
+  }
+
+  function resetHandle() {
+      dispatch(reset())
+  }
+
   return (
     <div>
       <h3 style={{color: 'white'}}>Всього: {total}</h3>
+      <h3 style={{color: 'white'}}>Гроші: {money}</h3>
       <button onClick={clearCartHandle}>Очистити</button>
+      <button onClick={buyCartHandle}>Купити</button>
+      <button onClick={resetHandle}>Рестарт</button>
       <BaseCardContainer>
         {data.map(({title, description, img, price}, index) => {
           return (
--- store/slices/goodsSlice.ts
+++ store/slices/goodsSlice.ts
@@ -7,37 +7,52 @@ interface GoodsState {
     total: number
     goods: IGoodsCard[]
     cart: boolean[]
+    money: number
 }
 
 const initialState: GoodsState = {
     total: 0,
     goods: goodsClothes,
-    cart: new Array(goodsClothes.length).fill(false)
+    cart: new Array(goodsClothes.length).fill(false),
+    money: 10_000
 }
 
 export const goodsSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
+        reset: () => initialState,
         toggleGoods: (state, action: PayloadAction<number>) => {
             state.cart[action.payload] = !state.cart[action.payload]
         },
         clearCart: (state) => {
-            state.cart = new Array(state.cart.length).fill(false)
+            state.cart = initialState.cart
+        },
+        buyCartOrFail: (state) => {
+            const totalPrice = getTotalPrice(state.cart)
+            if (state.money < totalPrice) {
+                throw new Error('Не достатньо коштів')
+            }
+            state.money -= totalPrice
+            state.cart = initialState.cart
         }
     },
 })
 
-export const totalPriceComputed = createSelector(
-    (state: { goods: GoodsState }) => state.goods.cart,
-    (cart) => cart.reduce((total, isInCart, index) => {
+function getTotalPrice(cart: boolean[]) {
+    return cart.reduce((total, isInCart, index) => {
         if (isInCart) {
             return total + Number(goodsClothes[index].price)
         } else {
             return total
         }
-    }, 0),
+    }, 0)
+}
+
+export const totalPriceComputed = createSelector(
+    (state: { goods: GoodsState }) => state.goods.cart,
+    (cart) => getTotalPrice(cart)
 );
 
-export const { toggleGoods, clearCart } = goodsSlice.actions
+export const { toggleGoods, reset, clearCart, buyCartOrFail } = goodsSlice.actions
 export default goodsSlice.reducer
